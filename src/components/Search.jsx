a0c3fd1a2381deea7a3684eeee1df5ec558db8c1
Adrian <adrianivsdk@gmail.com>
2019-10-04 15:31:18 -0400

    More styling, fixed inputs on home page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,19 +9,19 @@ class Search extends React.Component {
         <div className="container-fluid">
           <h1>Book unique places to stay and things to do.</h1>
           <p>Where:</p>
-          <input type="text"></input>
+          <input type="text" placeholder="Anywhere"></input>
           <div className="row">
-            <div className="col pr-0">
+            <div className="col pr-3">
               <p>Check In:</p>
-              <input type="text"></input>
+              <input type="date" placeholder="mm/dd/yyyy"></input>
             </div>
-            <div className="col pl-0">
+            <div className="col pl-3">
               <p>Check Out:</p>
-              <input type="text"></input>
+              <input type="date" placeholder="mm/dd/yyyy"></input>
             </div>
           </div>
           <p>Guests:</p>
-          <input type="text"></input>
+          <input type="text" placeholder=""></input>
           <button className="btn align-self-end"> Search </button>
         </div>
       </div>
